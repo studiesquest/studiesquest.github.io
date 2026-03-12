f97d2f6580a847d69afccff7cee5783d4ef2414b
studiesquest <nasimshavkatov23@gmail.com>
2026-03-12 19:13:04 -0400

    Update games.js

--- games.js
+++ games.js
@@ -1,178 +1,48 @@
-const genericThumbnail = "https://cdn-icons-png.flaticon.com/512/2933/2933181.png";
+const genericThumb = "https://cdn-icons-png.flaticon.com/512/2933/2933181.png";
 
 const games = [
-{
-    title: "Slope",
-    src: "https://zapgames.io/slope.embed",
-    thumbnail: "https://images.crazygames.com/slope_16x9/20250416101236/slope_16x9-cover?height=360",
-    description: "Dodge obstacles down a dangerous slope.",
-    signIn: "Playable",
-    normalChrome: "Playable"
-},
-{
-    title: "Smash Karts",
-    src: "https://zapgames.io/smash-karts.embed",
-    thumbnail: "https://images.crazygames.com/smash-karts_16x9/20250510101236/smash-karts_16x9-cover?height=360",
-    description: "Chaotic multiplayer kart battles.",
-    signIn: "Playable",
-    normalChrome: "Playable"
-},
-{
-    title: "Arrow Archers",
-    src: "https://zapgames.io/arrow-archers.embed",
-    thumbnail: "https://images.crazygames.com/archers_16x9/20250416101236/archers_16x9-cover?height=360",
-    description: "Aim and shoot archers in fast combat.",
-    signIn: "Slightly Unplayable",
-    normalChrome: "Playable"
-},
-{
-    title: "Drive Mad",
-    src: "https://zapgames.io/drive-mad.embed",
-    thumbnail: "https://images.crazygames.com/drive-mad_16x9/20250416101236/drive-mad_16x9-cover?height=360",
-    description: "Wreck cars and race through mayhem.",
-    signIn: "Playable",
-    normalChrome: "Playable"
-},
-{
-    title: "People Playground",
-    src: "https://azgames.io/ragdoll-playground.embed",
-    thumbnail: genericThumbnail,
-    description: "Sandbox physics ragdoll fun.",
-    signIn: "Hardly Playable",
-    normalChrome: "Playable"
-},
-{
-    title: "Minecraft Classic",
-    src: "https://classic.minecraft.net",
-    thumbnail: "https://upload.wikimedia.org/wikipedia/en/5/51/Minecraft_cover.png",
-    description: "Classic browser version of Minecraft.",
-    signIn: "Hardly Playable",
-    normalChrome: "Playable"
-},
-{
-    title: "Run 3",
-    src: "https://zapgames.io/run-3.embed",
-    thumbnail: "https://images.crazygames.com/run-3_16x9/20250416101236/run-3_16x9-cover?height=360",
-    description: "Run through space tunnels.",
-    signIn: "Playable",
-    normalChrome: "Playable"
-},
-{
-    title: "Subway Surfers",
-    src: "https://zapgames.io/subway-surfers.embed",
-    thumbnail: "https://images.crazygames.com/subway-surfers_16x9/20250416101236/subway-surfers_16x9-cover?height=360",
-    description: "Dodge trains and obstacles.",
-    signIn: "Playable",
-    normalChrome: "Playable"
-},
-{
-    title: "Tunnel Rush",
-    src: "https://zapgames.io/tunnel-rush.embed",
-    thumbnail: "https://images.crazygames.com/tunnel-rush_16x9/20250416101236/tunnel-rush_16x9-cover?height=360",
-    description: "Fast reflex tunnel dodging.",
-    signIn: "Playable",
-    normalChrome: "Playable"
-},
-{
-    title: "Retro Bowl",
-    src: "https://zapgames.io/retro-bowl.embed",
-    thumbnail: "https://images.crazygames.com/retro-bowl_16x9/20250416101236/retro-bowl_16x9-cover?height=360",
-    description: "Arcade football thrills.",
-    signIn: "Playable",
-    normalChrome: "Playable"
-},
-{
-    title: "Agar.io",
-    src: "https://zapgames.io/agar-io.embed",
-    thumbnail: "https://images.crazygames.com/agar-io_16x9/20250416101236/agar-io_16x9-cover?height=360",
-    description: "Grow your cell to dominate.",
-    signIn: "Playable",
-    normalChrome: "Playable"
-},
-{
-    title: "Slither.io",
-    src: "https://zapgames.io/slither-io.embed",
-    thumbnail: "https://images.crazygames.com/slither-io_16x9/20250416101236/slither-io_16x9-cover?height=360",
-    description: "Grow your snake in arena.",
-    signIn: "Playable",
-    normalChrome: "Playable"
-},
-{
-    title: "Paper.io 2",
-    src: "https://zapgames.io/paper-io-2.embed",
-    thumbnail: "https://images.crazygames.com/paper-io-2_16x9/20250416101236/paper-io-2_16x9-cover?height=360",
-    description: "Capture territory in this IO game.",
-    signIn: "Playable",
-    normalChrome: "Playable"
-},
-{
-    title: "Hole.io",
-    src: "https://zapgames.io/hole-io.embed",
-    thumbnail: "https://images.crazygames.com/hole-io_16x9/20250416101236/hole-io_16x9-cover?height=360",
-    description: "Grow larger by swallowing the world.",
-    signIn: "Playable",
-    normalChrome: "Playable"
-},
-{
-    title: "Stack Ball",
-    src: "https://zapgames.io/stack-ball.embed",
-    thumbnail: "https://images.crazygames.com/stack-ball_16x9/20250416101236/stack-ball_16x9-cover?height=360",
-    description: "Smash through platforms, fast!",
-    signIn: "Playable",
-    normalChrome: "Playable"
-},
-{
-    title: "Color Switch",
-    src: "https://zapgames.io/color-switch.embed",
-    thumbnail: "https://images.crazygames.com/color-switch_16x9/20250416101236/color-switch_16x9-cover?height=360",
-    description: "Match colors as you tap.",
-    signIn: "Playable",
-    normalChrome: "Playable"
-},
-{
-    title: "Crossy Road",
-    src: "https://zapgames.io/crossy-road.embed",
-    thumbnail: "https://images.crazygames.com/crossy-road_16x9/20250416101236/crossy-road_16x9-cover?height=360",
-    description: "Classic road dodge adventure.",
-    signIn: "Playable",
-    normalChrome: "Playable"
-},
-{
-    title: "Basket Random",
-    src: "https://zapgames.io/basket-random.embed",
-    thumbnail: "https://images.crazygames.com/basket-random_16x9/20250416101236/basket-random_16x9-cover?height=360",
-    description: "Basketball with unpredictable physics.",
-    signIn: "Playable",
-    normalChrome: "Playable"
-},
-{
-    title: "Soccer Random",
-    src: "https://zapgames.io/soccer-random.embed",
-    thumbnail: "https://images.crazygames.com/soccer-random_16x9/20250416101236/soccer-random_16x9-cover?height=360",
-    description: "Crazy soccer action.",
-    signIn: "Playable",
-    normalChrome: "Playable"
-},
-// … Continue adding additional games in same format up to 100+
-// You can expand with games from EmbedSites like MadKidGames where embed is provided  [oai_citation:0‡madkidgames.com](https://www.madkidgames.com/html5-games?utm_source=chatgpt.com)
+{title:"Slope", src:"https://zapgames.io/slope.embed", thumbnail:"https://zapgames.io/thumb/slope.png", description:"Dodge obstacles down the slope.", signIn:"Playable", normal:"Playable"},
+{title:"Slope Rider", src:"https://zapgames.io/slope-rider.embed", thumbnail:"https://zapgames.io/thumb/slope-rider.png", description:"Ride the slope in this endless challenge.", signIn:"Playable", normal:"Playable"},
+{title:"Smash Karts", src:"https://zapgames.io/smash-karts.embed", thumbnail:"https://zapgames.io/thumb/smash-karts.png", description:"Fast multiplayer kart battles.", signIn:"Playable", normal:"Playable"},
+{title:"Drive Mad", src:"https://zapgames.io/drive-mad.embed", thumbnail:"https://zapgames.io/thumb/drive-mad.png", description:"Crazy driving and destruction.", signIn:"Playable", normal:"Playable"},
+{title:"Ragdoll Archers", src:"https://zapgames.io/ragdoll-archers.embed", thumbnail:"https://zapgames.io/thumb/ragdoll-archers.png", description:"Aim and shoot in ragdoll combat.", signIn:"Slightly Unplayable", normal:"Playable"},
+{title:"Car Chaos", src:"https://zapgames.io/car-chaos.embed", thumbnail:"https://zapgames.io/thumb/car-chaos.png", description:"Cause mayhem with vehicles.", signIn:"Playable", normal:"Playable"},
+{title:"Basket Random", src:"https://zapgames.io/basket-random.embed", thumbnail:"https://zapgames.io/thumb/basket-random.png", description:"Fun physics basketball game.", signIn:"Playable", normal:"Playable"},
+{title:"Football Legends", src:"https://zapgames.io/football-legends.embed", thumbnail:"https://zapgames.io/thumb/football-legends.png", description:"Classic soccer action.", signIn:"Playable", normal:"Playable"},
+{title:"Basketball Legends", src:"https://zapgames.io/basketball-legends.embed", thumbnail:"https://zapgames.io/thumb/basketball-legends.png", description:"Basketball arcade gameplay.", signIn:"Playable", normal:"Playable"},
+{title:"Bonk.io", src:"https://zapgames.io/bonk-io.embed", thumbnail:"https://zapgames.io/thumb/bonk-io.png", description:"Physics multiplayer mayhem.", signIn:"Playable", normal:"Playable"},
+{title:"Snake.io", src:"https://zapgames.io/snake-io.embed", thumbnail:"https://zapgames.io/thumb/snake-io.png", description:"Grow your snake endlessly.", signIn:"Playable", normal:"Playable"},
+{title:"Duck Life", src:"https://zapgames.io/duck-life.embed", thumbnail:"https://zapgames.io/thumb/duck-life.png", description:"Train ducks to race.", signIn:"Playable", normal:"Playable"},
+{title:"Short Life", src:"https://zapgames.io/short-life.embed", thumbnail:"https://zapgames.io/thumb/short-life.png", description:"Platforming action with ragdoll physics.", signIn:"Playable", normal:"Playable"},
+{title:"Only Up!", src:"https://zapgames.io/only-up.embed", thumbnail:"https://zapgames.io/thumb/only-up.png", description:"Climb ever higher.", signIn:"Slightly Unplayable", normal:"Playable"},
+{title:"Wacky Flip", src:"https://zapgames.io/wacky-flip.embed", thumbnail:"https://zapgames.io/thumb/wacky-flip.png", description:"Flip through insane obstacles.", signIn:"Playable", normal:"Playable"},
+{title:"Color Rhythm", src:"https://zapgames.io/color-rhythm.embed", thumbnail:"https://zapgames.io/thumb/color-rhythm.png", description:"Tap to the beat.", signIn:"Playable", normal:"Playable"},
+{title:"Hyper Tunnel", src:"https://zapgames.io/hyper-tunnel.embed", thumbnail:"https://zapgames.io/thumb/hyper-tunnel.png", description:"Fast reflex tunnel dash.", signIn:"Playable", normal:"Playable"},
+{title:"Orbit Beats", src:"https://zapgames.io/orbit-beats.embed", thumbnail:"https://zapgames.io/thumb/orbit-beats.png", description:"Music‑based arcade fun.", signIn:"Playable", normal:"Playable"},
+{title:"Stack Rush", src:"https://zapgames.io/stack-rush.embed", thumbnail:"https://zapgames.io/thumb/stack-rush.png", description:"Smash through stacks.", signIn:"Playable", normal:"Playable"},
+{title:"Chase Rush", src:"https://zapgames.io/chase-rush.embed", thumbnail:"https://zapgames.io/thumb/chase-rush.png", description:"Chase down obstacles.", signIn:"Playable", normal:"Playable"},
+{title:"Mr Flip", src:"https://zapgames.io/mr-flip.embed", thumbnail:"https://zapgames.io/thumb/mr-flip.png", description:"Flip and jump challenges.", signIn:"Playable", normal:"Playable"},
+{title:"Ragdoll Arena", src:"https://zapgames.io/ragdoll-arena.embed", thumbnail:"https://zapgames.io/thumb/ragdoll-arena.png", description:"Multiplayer ragdoll battles.", signIn:"Slightly Unplayable", normal:"Playable"},
+{title:"Undead Corridor", src:"https://zapgames.io/undead-corridor.embed", thumbnail:"https://zapgames.io/thumb/undead-corridor.png", description:"Fight zombies in corridors.", signIn:"Slightly Unplayable", normal:"Playable"},
+{title:"Orbit Kick", src:"https://zapgames.io/orbit-kick.embed", thumbnail:"https://zapgames.io/thumb/orbit-kick.png", description:"Arcade kick action.", signIn:"Playable", normal:"Playable"},
+{title:"Poxel.io", src:"https://zapgames.io/poxel-io.embed", thumbnail:"https://zapgames.io/thumb/poxel-io.png", description:"Grow and battle.", signIn:"Playable", normal:"Playable"},
+{title:"Bloxd.io", src:"https://zapgames.io/bloxd-io.embed", thumbnail:"https://zapgames.io/thumb/bloxd-io.png", description:"IO battle arena.", signIn:"Playable", normal:"Playable"},
+{title:"Territorial.io", src:"https://zapgames.io/territorial-io.embed", thumbnail:"https://zapgames.io/thumb/territorial-io.png", description:"Territory conquest IO.", signIn:"Playable", normal:"Playable"},
+{title:"Build Now GG", src:"https://zapgames.io/build-now-gg.embed", thumbnail:"https://zapgames.io/thumb/build-now-gg.png", description:"Build and destroy.", signIn:"Playable", normal:"Playable"},
+{title:"Traffic Rally", src:"https://zapgames.io/traffic-rally.embed", thumbnail:"https://zapgames.io/thumb/traffic-rally.png", description:"Fast rally racing.", signIn:"Playable", normal:"Playable"},
+{title:"Return Man", src:"https://zapgames.io/return-man.embed", thumbnail:"https://zapgames.io/thumb/return-man.png", description:"Gridiron rush.", signIn:"Playable", normal:"Playable"},
+{title:"MineFun.io", src:"https://zapgames.io/minefun-io.embed", thumbnail:"https://zapgames.io/thumb/minefun-io.png", description:"Mine and survive.", signIn:"Playable", normal:"Playable"},
+{title:"City Brawl", src:"https://zapgames.io/city-brawl.embed", thumbnail:"https://zapgames.io/thumb/city-brawl.png", description:"Street fight brawl.", signIn:"Playable", normal:"Playable"},
+{title:"Escape Road", src:"https://zapgames.io/escape-road.embed", thumbnail:"https://zapgames.io/thumb/escape-road.png", description:"Road survival action.", signIn:"Playable", normal:"Playable"},
+{title:"Escape Road 2", src:"https://zapgames.io/escape-road-2.embed", thumbnail:"https://zapgames.io/thumb/escape-road-2.png", description:"More road survival.", signIn:"Playable", normal:"Playable"},
+{title:"Escape Road City 2", src:"https://zapgames.io/escape-road-city-2.embed", thumbnail:"https://zapgames.io/thumb/escape-road-city-2.png", description:"City survival ride.", signIn:"Playable", normal:"Playable"},
+{title:"Curve Rush", src:"https://zapgames.io/curve-rush.embed", thumbnail:"https://zapgames.io/thumb/curve-rush.png", description:"Fast futuristic racer.", signIn:"Playable", normal:"Playable"},
+{title:"Curve Rush 2", src:"https://zapgames.io/curve-rush-2.embed", thumbnail:"https://zapgames.io/thumb/curve-rush-2.png", description:"Even faster rush.", signIn:"Playable", normal:"Playable"},
+{title:"Tap Road", src:"https://zapgames.io/tap-road.embed", thumbnail:"https://zapgames.io/thumb/tap-road.png", description:"Tap to drive.", signIn:"Playable", normal:"Playable"},
+{title:"Golf Hit", src:"https://zapgames.io/golf-hit.embed", thumbnail:"https://zapgames.io/thumb/golf-hit.png", description:"Golf arcade game.", signIn:"Playable", normal:"Playable"},
+{title:"Mini Royale 2", src:"https://zapgames.io/mini-royale-2.embed", thumbnail:"https://zapgames.io/thumb/mini-royale-2.png", description:"Battle royale shooter.", signIn:"Hardly Playable", normal:"Playable"},
+{title:"Deadshot.io", src:"https://zapgames.io/deadshot-io.embed", thumbnail:"https://zapgames.io/thumb/deadshot-io.png", description:"Quick reflex shooter.", signIn:"Hardly Playable", normal:"Playable"},
+{title:"1v1.LOL", src:"https://zapgames.io/1v1.lol.embed", thumbnail:"https://zapgames.io/thumb/1v1.lol.png", description:"Build and shoot arena.", signIn:"Slightly Unplayable", normal:"Playable"},
+{title:"Ragdoll Playground", src:"https://azgames.io/ragdoll-playground.embed", thumbnail:"https://azgames.io/thumb/ragdoll-playground.png", description:"Sandbox physics fun.", signIn:"Hardly Playable", normal:"Playable"},
+// … continue similarly until reaching 100 games
 ];
-
-const container = document.getElementById("games");
-
-games.forEach(game => {
-    const div = document.createElement("div");
-    div.className = "game-tile";
-    div.dataset.title = game.title;
-    div.dataset.signin = game.signIn;
-    div.dataset.normal = game.normalChrome;
-
-    div.innerHTML = `
-        <img class="game-tile-img" data-src="${game.src}" src="${game.thumbnail}" alt="${game.title}">
-        <h2>${game.title}</h2>
-        <p>${game.description}</p>
-        <p><strong>For Sign-in Users:</strong> ${game.signIn}</p>
-        <p><strong>Normal Chrome:</strong> ${game.normalChrome}</p>
-    `;
-
-    container.appendChild(div);
-});
