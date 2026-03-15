230fe77b2d11be3fe8631a10075645a1ed8a05a6
studiesquest <nasimshavkatov23@gmail.com>
2026-03-14 21:27:41 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const grid = document.getElementById("gameGrid");
+const search = document.getElementById("search");
+
+let currentFilter = "all";
 
 function loadGames(){
 
@@ -8,6 +11,7 @@ games.forEach(game => {
 
 const tile = document.createElement("div");
 tile.className = "tile";
+tile.dataset.category = game.category;
 
 tile.innerHTML = `
 <img src="${game.image}">
@@ -15,11 +19,55 @@ tile.innerHTML = `
 <span class="playability">${game.playability}</span>
 `;
 
-tile.onclick = () => {
-window.location.href = game.url;
-};
+tile.onclick = () => launchGame(game.url);
 
 grid.appendChild(tile);
+
+});
+
+}
+
+function launchGame(url){
+
+const player = document.getElementById("gamePlayer");
+
+player.innerHTML = `
+<iframe src="${url}" frameborder="0" allowfullscreen></iframe>
+`;
+
+document.getElementById("playerSection").style.display = "block";
+
+}
+
+function filterGames(category){
+
+currentFilter = category;
+
+document.querySelectorAll(".tile").forEach(tile => {
+
+if(category === "all" || tile.dataset.category === category){
+tile.style.display = "block";
+}else{
+tile.style.display = "none";
+}
+
+});
+
+}
+
+function searchGames(){
+
+const value = search.value.toLowerCase();
+
+document.querySelectorAll(".tile").forEach(tile => {
+
+const title = tile.querySelector("h3").innerText.toLowerCase();
+
+if(title.includes(value)){
+tile.style.display = "block";
+}else{
+tile.style.display = "none";
+}
 
 });
 
